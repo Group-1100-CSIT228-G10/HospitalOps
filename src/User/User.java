f1716b8593520fd95f3687290b97d62ceebce4fe
Julius <isaiahmancao@gmail.com>
2026-04-28 13:08:59 +0800

    initialized user

--- src/User/User.java
+++ src/User/User.java
@@ -1,5 +1,14 @@
 package User;
 
 public class User{
+    public String firstName;
+    public String lastName;
+    public String middleName;
+    public String gender;
+    public String city;
+    public String country;
+    public String birthDate;
+    public int age;
 
+    public User(){}
 }
