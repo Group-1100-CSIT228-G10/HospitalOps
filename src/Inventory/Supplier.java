3759a729f95af53b02ad07e700c9d458fd7e3d06
Matthew Robillos <matthewjohn.robillos@cit.edu>
2026-04-28 14:46:35 +0800

    Supplier Class

---
+++ src/Inventory/Supplier.java
@@ -0,0 +1,11 @@
+package Inventory;  
+
+public class Supplier {
+    public String name;
+    public String contactInfo;
+    
+    public Supplier(String name, String contactInfo) {
+        this.name = name;
+        this.contactInfo = contactInfo;
+    }
+}
