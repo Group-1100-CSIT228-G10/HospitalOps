128f508b6a49133717b19e919d51b0140f9bfab7
Jess <jessvenzel.casinto@cit.edu>
2026-04-28 21:50:16 +0800

    Improve Consultation class by adding date field and isCompleted status to track consultation completion

--- src/Consultation_Logs/Consultation.java
+++ src/Consultation_Logs/Consultation.java
@@ -3,6 +3,7 @@ package Consultation_Logs;
 import User.Doctor;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Consultation {
 
@@ -10,18 +11,26 @@ public class Consultation {
     private Patient patient;
     private String diagnosis;
     private List<UsageLog> usageLogs;
+    private Date date;
+    private boolean isCompleted;
 
     public Consultation(Doctor doctor, Patient patient, String diagnosis) {
         this.doctor = doctor;
         this.patient = patient;
         this.diagnosis = diagnosis;
         this.usageLogs = new ArrayList<>();
+        this.date = new Date();
+        this.isCompleted = false;
     }
 
     public void addUsageLog(UsageLog log) {
         usageLogs.add(log);
     }
 
+    public void completeConsultation() {
+        this.isCompleted = true;
+    }
+    
     public Doctor getDoctor() {
         return doctor;
     }
@@ -36,5 +45,21 @@ public class Consultation {
 
     public List<UsageLog> getUsageLogs() {
         return usageLogs;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public String getDoctorFullName() {
+        return doctor.getFirstName() + " " + doctor.getMiddleName() + " " + doctor.getLastName();
+    }
+
+    public String getPatientFullName() {
+        return patient.getFirstName() + " " + patient.getMiddleName() + " " + patient.getLastName();
     }
 }
