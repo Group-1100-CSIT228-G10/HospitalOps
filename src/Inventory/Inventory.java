6285a231f096c09d6193f63d8224883cde6798d6
Matthew Robillos <matthewjohn.robillos@cit.edu>
2026-04-28 15:15:50 +0800

    Add Inventory Class

--- src/Inventory/Inventory.java
+++ src/Inventory/Inventory.java
@@ -1,7 +1,15 @@
 package Inventory;
 
-public class Inventor {
+public class Inventory { 
     public String name;
-    public String description;
+    public String description; 
+    public int quantity;
     
+    public Inventory(String name, String description, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    } 
+
+
 }
