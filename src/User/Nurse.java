c6340733f7a742a69cc68a4437aabe0b20df04c5
Jess <jessvenzel.casinto@cit.edu>
2026-04-28 15:26:11 +0800

    Implemented Nurse class

--- src/User/Nurse.java
+++ src/User/Nurse.java
@@ -1,5 +1,15 @@
 package User;
 
-public class Nurse{
+public class Nurse extends User {
 
+    private String department;
+
+    public Nurse(String firstName, String lastName, String middleName, String gender, String city, String country, String birthDate, int age, String department) {
+        super(firstName, lastName, middleName, gender, city, country, birthDate, age);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
