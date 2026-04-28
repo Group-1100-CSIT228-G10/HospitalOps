d3a88a93b2f776aa08ff85a03a0e67ae47c454d2
Jess <jessvenzel.casinto@cit.edu>
2026-04-28 14:54:26 +0800

    Implemented User class

--- src/User/User.java
+++ src/User/User.java
@@ -1,14 +1,44 @@
 package User;
 
-public class User{
-    public String firstName;
-    public String lastName;
-    public String middleName;
-    public String gender;
-    public String city;
-    public String country;
-    public String birthDate;
-    public int age;
+public class User {
 
-    public User(){}
+    protected String firstName;
+    protected String lastName;
+    protected String middleName;
+    protected String gender;
+    protected String city;
+    protected String country;
+    protected String birthDate;
+    protected int age;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String middleName,
+                String gender, String city, String country,
+                String birthDate, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.gender = gender;
+        this.city = city;
+        this.country = country;
+        this.birthDate = birthDate;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
