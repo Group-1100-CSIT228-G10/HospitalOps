ecd86f4033dd1472dcf0fb11c583cea116294086
Julius Isaiah Mancao <isaiahmancao@gmail.com>
2026-05-04 12:38:07 +0800

    Revert "Usage_Log Folder and User Folder Initialization"

--- src/User/Admin.java
+++ src/User/Admin.java
@@ -1,15 +1,5 @@
 package User;
 
-public class Admin extends User {
+public class Admin{
 
-    private String role;
-
-    public Admin(String firstName, String lastName, String middleName, String gender, String city, String country, String birthDate, int age, String role) {
-        super(firstName, lastName, middleName, gender, city, country, birthDate, age);
-        this.role = role;
-    }
-
-    public String getRole() {
-        return role;
-    }
 }
