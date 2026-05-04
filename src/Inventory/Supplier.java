18f46633eaac57c30a68592ee03a585c215eeb5d
Matthew Robillos <matthewjohn.robillos@cit.edu>
2026-05-04 13:31:51 +0800

    Add a display on supplier class

--- src/Inventory/Supplier.java
+++ src/Inventory/Supplier.java
@@ -1,11 +1,20 @@
-package Inventory;  
+package Inventory;
 
 public class Supplier {
+
+    public int supplierID;
     public String name;
     public String contactInfo;
-    
-    public Supplier(String name, String contactInfo) {
+
+    public Supplier(int supplierID, String name, String contactInfo) {
+        this.supplierID = supplierID;
         this.name = name;
         this.contactInfo = contactInfo;
+    }
+
+    public void displaySupplier() {
+        System.out.println("Supplier ID: " + supplierID);
+        System.out.println("Name: " + name);
+        System.out.println("Contact: " + contactInfo);
     }
 }
