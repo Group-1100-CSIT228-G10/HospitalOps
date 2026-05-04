ba2a2340dc0305ab4f62208bef6d5f13b75816d3
Matthew Robillos <matthewjohn.robillos@cit.edu>
2026-05-04 13:30:36 +0800

    Add StockTransaction class with transaction and item linkage

--- src/Inventory/StockTransaction.java
+++ src/Inventory/StockTransaction.java
@@ -1,13 +1,36 @@
 package Inventory;
 
 public class StockTransaction {
-    public String name;
-    public String description;
-    public int quantity;
-    
-    public Item(String name, String description, int quantity) {
-        this.name = name;
-        this.description = description;
-        this.quantity = quantity;
+
+    private int transactionID;
+    private String type; 
+    private int quantityChanged;
+    private Date date;
+    private Item item;
+
+    public StockTransaction(int transactionID, String type, int quantityChanged, Item item) {
+        this.transactionID = transactionID;
+        this.type = type;
+        this.quantityChanged = quantityChanged;
+        this.item = item;
+        this.date = new Date();
+    }
+
+    public void applyTransaction() {
+        if (type.equalsIgnoreCase("IN")) {
+            item.addStock(quantityChanged);
+        } else if (type.equalsIgnoreCase("OUT")) {
+            item.reduceStock(quantityChanged);
+        } else {
+            System.out.println("Invalid transaction type!");
+        }
+    }
+
+    public void displayTransaction() {
+        System.out.println("Transaction ID: " + transactionID);
+        System.out.println("Type: " + type);
+        System.out.println("Quantity: " + quantityChanged);
+        System.out.println("Date: " + date);
+        System.out.println("Item: " + item.name);
     }
 }
