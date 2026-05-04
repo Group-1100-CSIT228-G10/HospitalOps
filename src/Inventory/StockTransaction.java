be555327da0afde5a449714aec8d73672f31c242
Jess <jessvenzel.casinto@cit.edu>
2026-05-04 12:25:17 +0800

    Update StockTransaction with new fields and getters

--- src/Inventory/StockTransaction.java
+++ src/Inventory/StockTransaction.java
@@ -1,13 +1,42 @@
 package Inventory;
 
-public class Item {
-    public String name;
-    public String description;
-    public int quantity;
-    
-    public Item(String name, String description, int quantity) {
-        this.name = name;
-        this.description = description;
+import java.util.Date;
+
+public class StockTransaction {
+
+    public enum TransactionType {
+        IN, OUT
+    }
+
+    private Item item;
+    private int quantity;
+    private TransactionType type;
+    private Date date;
+
+    public StockTransaction(Item item, int quantity, TransactionType type) {
+        this.item = item;
         this.quantity = quantity;
+        this.type = type;
+        this.date = new Date();
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String toString() {
+        return type + " - " + item.name + " - " + quantity;
     }
 }
