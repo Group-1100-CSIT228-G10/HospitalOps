de7ba63c874d13d8ba65f71e99717f6a282e2a7c
Matthew Robillos <matthewjohn.robillos@cit.edu>
2026-05-04 12:58:00 +0800

    Updated Item Class

--- src/Inventory/Item.java
+++ src/Inventory/Item.java
@@ -1,11 +1,13 @@
 package Inventory;
 
 public class Item {
+    public int itemID;
     public String name;
     public String description;
     public int quantity;
-    
-    public Item(String name, String description, int quantity) {
+
+    public Item(int itemID, String name, String description, int quantity) {
+        this.itemID = itemID;
         this.name = name;
         this.description = description;
         this.quantity = quantity;
