ee109c420292ad234f7c3a76fbf33881b1b99ccc
Jess <jessvenzel.casinto@cit.edu>
2026-05-06 01:55:55 +0800

    Add processed flag in UsageLog to prevent duplicates

--- src/Consultation_Logs/UsageLog.java
+++ src/Consultation_Logs/UsageLog.java
@@ -1,25 +1,34 @@
 package Consultation_Logs;
- 
+
 public class UsageLog {
- 
+
     private String itemName;
     private int quantity;
- 
+    private boolean processed = false;
+
     public UsageLog() {}
- 
+
     public UsageLog(String itemName, int quantity) {
         this.itemName = itemName;
         this.quantity = quantity;
     }
- 
+
     public String getItemName() {
         return itemName;
     }
- 
+
     public int getQuantity() {
         return quantity;
     }
- 
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public void markProcessed() {
+        this.processed = true;
+    }
+
     public String toString() {
         return itemName + " - " + quantity;
     }
