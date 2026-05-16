76524477448b0941a1b135ef864e61adf45c713b
MattYou1 <matthewjohn.robillos@cit.edu>
2026-05-16 23:30:14 +0800

    05/16/2026

--- src/Consultation_Logs/Consultation.java
+++ src/Consultation_Logs/Consultation.java
@@ -7,23 +7,45 @@ import Inventory.Item;
 public class Consultation {
 
     private Patient patient;
+    private ArrayList<String> symptoms;
     private String diagnosis;
-    private Date date = new Date();
+    private Date date;
     private boolean isCompleted = false;
     private UsageLog usageLog;
 
     public Consultation(Patient patient) {
         this.patient = patient;
+        symptoms = new ArrayList<>();
         diagnosis = "";
+        date = new Date();
         usageLog = null;
     }
 
     public Patient getPatient() {
+        if(patient == null){
+            System.out.println("No patient assigned to this consultation.");
+        }
         return patient;
     }
 
-    public String getDiagnosis() {
-        return diagnosis;
+    public void addSymptom(String symptom) {
+        symptoms.add(symptom);
+    }
+
+    public void getSymptoms() {
+        System.out.println("Symptoms for patient " + patient.getFirstName() + " " + patient.getLastName() + ":");
+        for (String symptom : symptoms) {
+            System.out.println(symptom);
+        }
+    }
+
+
+    public void InitializeUsageLog(String logName) {
+        if(usageLog == null){
+            this.usageLog = new UsageLog(logName);
+        }else{
+            System.out.println("Usage log already initialized for this consultation.");
+        }
     }
 
     public boolean updateUsageLog(Item item) {
@@ -37,28 +59,23 @@ public class Consultation {
 
     }
 
-    public void InitializeUsageLog(String logName) {
-        if(usageLog == null){
-            this.usageLog = new UsageLog(logName);
-        }else{
-            System.out.println("Usage log already initialized for this consultation.");
-        }
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
     public boolean isCompleted() {
         return isCompleted;
+    }
+
+    
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    
+    public String getDiagnosis() {
+        return diagnosis;
     }
 
     public void setDiagnosis(String diagnosis) {
         this.diagnosis = diagnosis;
     }
 
-    public void setCompleted(boolean completed) {
-        isCompleted = completed;
-    }
 
 }
